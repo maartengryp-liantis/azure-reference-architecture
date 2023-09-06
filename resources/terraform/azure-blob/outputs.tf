e65b3b2e383e83974622337d1167edff884ce72a
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-09-06 18:32:31 -0400

    no encode

--- resources/terraform/azure-blob/outputs.tf
+++ resources/terraform/azure-blob/outputs.tf
@@ -7,10 +7,10 @@ output "account" {
 }
 
 output "encoded_account" {
-  value = base64encode(azurerm_storage_account.storage_account.name)
+  value = azurerm_storage_account.storage_account.name
 }
 
 output "account_access_key" {
-  value = base64encode(azurerm_storage_account.storage_account.primary_access_key)
+  value     = azurerm_storage_account.storage_account.primary_access_key
   sensitive = true
 }
