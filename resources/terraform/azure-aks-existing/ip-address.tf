89cad799a188eac20aceb8b686617ce13f44fa7e
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2023-09-19 10:33:39 -0400

    Update ip-address.tf

--- resources/terraform/azure-aks-existing/ip-address.tf
+++ resources/terraform/azure-aks-existing/ip-address.tf
@@ -1,5 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/data-sources/public_ip
 data "azurerm_public_ip" "public_ip" {
-  name                = var.public_ip_address_name
+  name                = var.ingress_nginx_public_ip_address
   resource_group_name = var.resource_group_name
 }
